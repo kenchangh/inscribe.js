d88fea6f74b001172dd30a6b1e8a65cb58eb682a
mavenave <guanhao3797@gmail.com>
2014-12-10 23:42:25 +0800

    Modified build system to fulfill current needs

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
 var gulp = require('gulp');
 var server = require('gulp-express');
 
-gulp.task('default', function() {});
+var paths = {
+  jade: './demo/views/*.jade',
+  js: './demo/**/*.js',
+  inscribe: './demo/public/javascripts/inscribe.js'
+};
 
 gulp.task('server', function() {
   // start the server at beginning of task
   function runServer() {
     server.run({
-      file: './test/bin/www',
+      file: './demo/bin/www',
     });
   }
   runServer();
 
   // server.notify reloads at browser
   // server.run restarts server
-  gulp.watch('./test/views/*.jade', server.notify);
-  gulp.watch('./test/routes/*.js', runServer);
-  gulp.watch('./test/public/javascripts/*.js', runServer);
-  gulp.watch('./test/app.js', runServer);
+  gulp.watch(paths.jade, server.notify);
+  gulp.watch(paths.js, runServer);
 });
+
+gulp.task('move inscribe', function() {
+  gulp.src(paths.inscribe)
+    .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('default', ['server'], function() {
+  gulp.watch(paths.inscribe, ['move inscribe']);
+});
+
